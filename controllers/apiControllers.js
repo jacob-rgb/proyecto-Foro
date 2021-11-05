88f5511abe2faa9243a1515562a2715612515852
DESKTOP-H976RTI\jacob <jacoboramirezrivera6@gmail.com>
2021-11-05 16:31:46 +0100

    likessyste7

--- controllers/apiControllers.js
+++ controllers/apiControllers.js
@@ -169,7 +169,7 @@ const controller = {
         Post.findByIdAndUpdate( postId, {$inc: { likes: 1 }},{ timestamps: false } ).exec((err, post) => {
             if(err) return res.status(500).send('Error al devolver los datos');
             if(!post) return res.status(404).send('No hay Proyectos para mostrar');
-            
+            console.log('hola');
             await userModel.findByIdAndUpdate(post.userId, { $push: {"likes.likes": postId}});
             return res.status(200).send({post: post});
         })
